82155f18df4f9952baf2127d4680c9f47849a0c6
Debabrata Patnaik <debabrata_patnaik@live.com>
2024-11-29 12:42:13 +0530

    change shell to sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
 if [ "$INPUT_CLUSTER_MODE" = "false" ]; then
-  docker-compose -f docker-compose-single.yml up
+  docker-compose -f docker-compose-single.yml up -d
 else
-  docker-compose -f docker-compose-cluster.yml up
+  docker-compose -f docker-compose-cluster.yml up -d
 fi
