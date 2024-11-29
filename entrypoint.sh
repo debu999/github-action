7fa549c4ad5bea81f63ee30a335ba25eb66a8d96
Debabrata Patnaik <debabrata_patnaik@live.com>
2024-11-29 12:53:53 +0530

    move to docker compose v2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$INPUT_CLUSTER_MODE" = "false" ]; then
-  docker-compose -f docker-compose-single.yml up -d
+  docker compose -f docker-compose-single.yml up -d
 else
-  docker-compose -f docker-compose-cluster.yml up -d
+  docker compose -f docker-compose-cluster.yml up -d
 fi
