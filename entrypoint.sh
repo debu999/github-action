e04a0c248c9ac05bcad2687d7d12830d86c42a3b
Debabrata Patnaik <debabrata_patnaik@live.com>
2024-11-29 13:11:36 +0530

    fix env var

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-echo $INPUT_VERSION
-echo ${INPUT_CLUSTER-MODE}
+echo "$INPUT_VERSION"
+echo "${INPUT_CLUSTER-MODE}"
 
 if [ "${INPUT_CLUSTER-MODE}" = "false" ]; then
   run_redpanda="docker compose -f docker-compose-single.yml up -d"
